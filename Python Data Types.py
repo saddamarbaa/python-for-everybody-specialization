dfdc7a0bdbfc1409cf02e4957489be341ed9df6a
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-23 19:54:54 +0700

    Python Basic Data Types

--- Python Data Types.py
+++ Python Data Types.py
@@ -1,1 +1,21 @@
+# Python Data Types
+var = 100           # int type
+print(type(var))    # <class 'int'>
+var = "my name"      # string
+print(type(var))     # <class 'str'>
+z = 4J 
+print(type(z))     # <class 'complex'>
+# declaration string
+name = 'sadam'
+job = "Web Programmer"
+message = " this is my pyhton code"
+# printing string
+print('Name: ', name)
+print('Job: ', job)
+print('Message: ', message)
 
+var = True # declaration bool
+if (var == True):
+    print("var == true")
+else :
+     print("var == False")
