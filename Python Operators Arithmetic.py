02fc1c3c4eb415ea16a2b337ba6ce9d039ef3947
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-24 03:39:44 +0700

    Python Arithmetic Operators

--- Python Operators Arithmetic.py
+++ Python Operators Arithmetic.py
@@ -1,1 +1,29 @@
-
+# Python Arithmetic Operators
+# variables
+firstNumber = 12
+secondNumber = 13
+# Addition
+print(firstNumber + secondNumber)
+print(firstNumber + 3)
+firstNumber = 5
+secondNumber = 2
+# Subtraction
+print(firstNumber - secondNumber)
+print(secondNumber - 2)
+firstNumber = 25
+secondNumber = 5
+# Division
+print(firstNumber / secondNumber)
+print(secondNumber / 2)
+# Floor division
+print(firstNumber // secondNumber)
+print(firstNumber // 2)
+# Multiplication
+print(firstNumber * secondNumber)
+print(firstNumber * secondNumber)
+print(secondNumber * 2)
+# Exponentiation
+firstNumber = 5
+secondNumber = 3
+print(firstNumber ** 2)
+print(firstNumber * secondNumber)
