bb1e3c798b556b84b0db0ad267335b84edd2c6a4
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-24 04:20:06 +0700

    Python Comparison Operators

--- Python Operators Comparison .py
+++ Python Operators Comparison .py
@@ -1,1 +1,27 @@
- 
+ # Python Comparison Operators
+a = 12
+b = 23
+
+# (==) Equal Operator
+print(a == b)     # returns False because 12 is not equal to 23
+print(23 == b)    # returns true because 23is is equal to 23
+
+# (!=) Not equal Operator
+print(a != b)      # returns true because 12 is not equal to 23
+print(23 != b)    # returns false because 23 is is equal to 23
+
+# (>) Greater than Operator
+print(a > b)      # returns false because 12 is not greater than 23
+print(24 > b)    # returns true because 24 is is greater than 23
+
+# (<) Less than Operator
+print(a < b)      # returns true because 12 is less than 23
+print(24 < b)    # returns false because 24 is not less than 23
+
+# (>=) Greater than or equal  Operator
+print(a >= b)      # returns false because 12 is not greater, or equal, to 23
+print(24 >= b)    # returns True because 24 is greater, or equal, to 23
+
+# (<=) Less than or equal to  Operator
+print(a <= b)      # returns true because 12 is Less, or equal, to 23
+print(24 <= b)    # returns False because 24 is neither less than or equal to 23
