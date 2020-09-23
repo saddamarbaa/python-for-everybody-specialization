138f13a0629173b7d9f2d65cedf781ef65fd73ad
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-23 22:22:22 +0700

    Python Program to swap two variables

--- Python swap.py
+++ Python swap.py
@@ -1,1 +1,13 @@
-
+# Python program to swap two variables
+v1 = "first string"
+v2 = "second string"
+# before swap
+print("v1 : ", v1)
+print("v2 : ", v2)
+# create a temporary variable and swap the values
+temp = v1
+v1 = v2
+v2 = temp
+# after swap
+print("v1 : ", v1)
+print("v2 : ", v2)
