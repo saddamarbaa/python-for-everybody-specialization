fbda8b94526f160aa02d230231ebd55620f701b2
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-24 05:01:11 +0700

    Simple Program to Calculating an average

---
+++ Python user input average.py
@@ -0,0 +1,8 @@
+# simple program to Calculating an average
+numberOfScoures = 3
+score1 = float(input("Enter Scoure 1: "))
+score2 = float(input("Enter Scoure 2: "))
+score3 = float(input("Enter Scoure 3: "))
+# Calculate average
+average = (score1 + score2 + score3) / numberOfScoures
+print("the average is", average)
