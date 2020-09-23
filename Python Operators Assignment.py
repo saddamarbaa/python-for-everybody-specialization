e14c455c590f447edd9fc8dfbdc736dbcd17c436
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-24 04:01:19 +0700

    Python Assignment Operators

---
+++ Python Operators Assignment.py
@@ -0,0 +1,44 @@
+# Python Assignment Operators
+num = 12 # variable
+# = Operator
+num = 15
+# += Operator
+num += 3 # num = num +1
+print(num)
+# -= Operator
+num -= 3 # num = num -3
+print(num)
+# *= Operator
+num *= 5 # num = num * 5
+print(num)
+# /= Operator
+num /= 5 # num = num / 5
+print(num)
+# %= Operator
+num %= 5 # num = num % 5
+print(num)
+# **= Operator
+num = 2
+num **= 5 # num = num ** 5
+print(num)
+# &= Operator
+num = 2
+num &= 5 # num = num & 5
+print(num)
+# |= Operator
+num = 12
+num |= 5 # num = num | 5
+print(num)
+# ^= Operator
+num = 12
+num ^= 5 # num = num ^ 5
+print(num)
+# >>= Operator
+num = 12
+num >>= 5 # num = num >> 5
+print(num)
+# <<= Operator
+num = 12
+num <<= 5 # num = num << 5
+print(num)
+
