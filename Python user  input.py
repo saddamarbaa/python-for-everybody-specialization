f59f2114973fb04b972637f999fd66b4a4dd3a7b
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-24 03:24:11 +0700

    Reading keyboard input (User Information)

--- Python user  input.py
+++ Python user  input.py
@@ -1,1 +1,10 @@
-
+# user input
+userName = input("Please Enter Your Name: ")
+userInputString = input("Please Enter Your age: ")
+# convert userInputString to int
+userAge = int(userInputString)
+userInputString = input("Please Enter How much you make yearly: ")
+# convert userInputString to float
+userYearlyWage = float(userInputString )
+# display user information
+print(userName, "age", userAge, "make", userYearlyWage, "yearly.")
