3c39a853fd39ac74c059b8ad12c1ccdd36f308cd
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-23 20:39:45 +0700

    Python Numbers

--- Python Numbers.py
+++ Python Numbers.py
@@ -1,1 +1,24 @@
-
+# Python Numbers
+# Int :
+x = 3
+y = 237172634
+z = -7172634
+print(type(x))     #<class 'int'>
+print(type(y))     #<class 'int'>
+print(type(z))     #<class 'int'>
+# Float :
+x = 3.5
+y = 0.2
+z = -32.1
+m = 12E4
+s = 243.6e65
+print(type(x))     # <class 'float'>
+print(type(y))     # <class 'float'>
+print(type(z))     # <class 'float'>
+# Complex:
+x = 7 + 7j
+y = 18j
+z = -21j
+print(type(x))    # <class 'complex'>
+print(type(y))    # <class 'complex'>
+print(type(z))    # <class 'complex'>
