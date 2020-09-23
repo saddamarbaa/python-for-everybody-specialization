f9adea553ccfe18044200caa3046e9eca1d0f6d4
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-23 19:31:00 +0700

    Python Variables

--- Python Variables.py
+++ Python Variables.py
@@ -1,1 +1,16 @@
+# Python Variables
 
+x = 42 # x is of type int
+x = "ALI" # x is now of type str
+var = 5 # declar var of type int 5
+
+c = y = z = 110 # Multiple Assignment
+print(x)
+print('c = ', c)
+print('y = ', y)
+print('z = ', z)
+
+name = "Ali"
+# is same as blow
+name = 'Ali'
+print(name)
