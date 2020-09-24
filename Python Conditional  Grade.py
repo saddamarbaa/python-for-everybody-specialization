4a82e636e4c84ca486a66498ba08d8e56c9b605a
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-25 06:09:44 +0700

    Python Program Code to Calculate Grade of Student

--- Python Conditional  Grade.py
+++ Python Conditional  Grade.py
@@ -1,1 +1,17 @@
-
+# Python Program Code to Calculate Grade of Student
+score = input("Enter Score: ")
+scoure = float(score) # convert
+if scoure > 0.0 and  scoure < 1.0 :
+    if scoure >= 0.9 :
+        print("A")
+    elif scoure >= 0.8 :
+        print("B") 
+    elif scoure >= 0.7 :
+        print("C")
+    elif scoure >= 0.6 :
+            print("D") 
+    else:
+        print("F")
+else :
+    print("Error number is out of range")
+        
