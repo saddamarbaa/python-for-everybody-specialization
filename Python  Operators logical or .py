99832e032f125934c07f8bda34699ca37bef8889
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-25 18:38:56 +0700

    The Logical AND Operator, Example Program

--- Python  Operators logical or .py
+++
@@ -1,8 +0,0 @@
-# The or logical operator, example program 
-# 13,14,15,16,17,18,19
-userAge = int(input("Please Enter Age: "))
-if userAge < 13 or userAge > 19:
-    print("You are not teenger!")
-else :
-    print("You are Teenger")
-    
