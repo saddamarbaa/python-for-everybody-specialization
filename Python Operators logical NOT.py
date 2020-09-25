e4012750a70875f0fd6025714bccfa73f2b811b6
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-25 18:51:10 +0700

    The NOT Logical Operator, Example

---
+++ Python Operators logical NOT.py
@@ -0,0 +1,9 @@
+# The not logical Operator, Example 
+ADULT_AGE = 18
+userAge = int(input("Please Enter Age: "))
+if not(userAge >= ADULT_AGE):
+    print("You are  NOT adult!")
+else :
+    print("You are adult!")
+    
+    
